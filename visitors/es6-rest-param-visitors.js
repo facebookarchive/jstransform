84d1d0a016a2818e904cef442e644471c56413ad
Kaustubh Karkare <kaustubh.karkare@gmail.com>
2015-02-10 19:55:24 -0800

    Inject temporary variables at start of scope.

--- visitors/es6-rest-param-visitors.js
+++ visitors/es6-rest-param-visitors.js
@@ -80,8 +80,8 @@ function renderRestParamSetup(functionNode, state) {
   var len = state.localScope.tempVarIndex++;
 
   return 'for (var ' + functionNode.rest.name + '=[],' +
-    utils.getTempVarWithValue(idx, functionNode.params.length) + ',' +
-    utils.getTempVarWithValue(len, 'arguments.length') + ';' +
+    utils.getTempVar(idx) + '=' + functionNode.params.length + ',' +
+    utils.getTempVar(len) + '=arguments.length;' +
     utils.getTempVar(idx) + '<' +  utils.getTempVar(len) + ';' +
     utils.getTempVar(idx) + '++) ' +
     functionNode.rest.name + '.push(arguments[' + utils.getTempVar(idx) + ']);';
