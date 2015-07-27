c4c9b3036d1169436d109587b30e89ea0410ea77
thr0w <josefernandotolentino@gmail.com>
2015-07-27 19:54:12 -0300

    Travis

--- visitors/react-jsx-module-visitors.js
+++ visitors/react-jsx-module-visitors.js
@@ -106,100 +106,21 @@ function visitReactJSXClassDeclaration(traverse, node, path, state) {
     }
   });
 
-  if (error)
-     throw new Error(error +'. (line: ' +
-       node.loc.start.line + ', col: ' + node.loc.start.column + ')'
-     );
-  if (export_class === 'default')
+  if (error) {
+    throw new Error(error +'. (line: ' +
+      node.loc.start.line + ', col: ' + node.loc.start.column + ')'
+    );
+  }
+  if (export_class === 'default') {
     utils.append('\nexport default '+state.className.name+';', state);
-  else if (export_class === true)
+  }
+  else if (export_class === true) {
     utils.append('\nexport '+state.className.name+';', state);
-  //utils.catchupWhiteSpace(node.range[1], state);
-  //utils.catchupWhiteSpace(node.range[1], state);
-
-  //_renderClassBody(traverse, node, path, state);
+  }
 
   return false;
 
 }
-
-/**
- * @param {function} traverse
- * @param {object} node
- * @param {array} path
- * @param {object} state
- */
-function _renderClassBody(traverse, node, path, state) {
-  var className = state.className;
-
-//  utils.append(
-//    'var ' + superClass.name + '=' + superClass.expression + ';',
-//    state
-//  );
-//
-//
-//    var keyName = superClass.name + '____Key';
-//    var keyNameDeclarator = '';
-//    if (!utils.identWithinLexicalScope(keyName, state)) {
-//      keyNameDeclarator = 'var ';
-//      declareIdentInLocalScope(keyName, initScopeMetadata(node), state);
-//    }
-//    utils.append(
-//      'for(' + keyNameDeclarator + keyName + ' in ' + superClass.name + '){' +
-//        'if(' + superClass.name + '.hasOwnProperty(' + keyName + ')){' +
-//          className + '[' + keyName + ']=' +
-//            superClass.name + '[' + keyName + '];' +
-//        '}' +
-//      '}',
-//      state
-//    );
-//
-//    var superProtoIdentStr = SUPER_PROTO_IDENT_PREFIX + superClass.name;
-//    if (!utils.identWithinLexicalScope(superProtoIdentStr, state)) {
-//      utils.append(
-//        'var ' + superProtoIdentStr + '=' + superClass.name + '===null?' +
-//        'null:' + superClass.name + '.prototype;',
-//        state
-//      );
-//      declareIdentInLocalScope(superProtoIdentStr, initScopeMetadata(node), state);
-//    }
-//
-//    utils.append(
-//      className + '.prototype=Object.create(' + superProtoIdentStr + ');',
-//      state
-//    );
-//    utils.append(
-//      className + '.prototype.constructor=' + className + ';',
-//      state
-//    );
-//    utils.append(
-//      className + '.__superConstructor__=' + superClass.name + ';',
-//      state
-//    );
-//  }
-//
-//  // If there's no constructor method specified in the class body, create an
-//  // empty constructor function at the top (same line as the class keyword)
-//  if (!node.body.body.filter(_isConstructorMethod).pop()) {
-//    utils.append('function ' + className + '(){', state);
-//    if (!state.scopeIsStrict) {
-//      utils.append('"use strict";', state);
-//    }
-//    if (superClass.name) {
-//      utils.append(
-//        'if(' + superClass.name + '!==null){' +
-//        superClass.name + '.apply(this,arguments);}',
-//        state
-//      );
-//    }
-//    utils.append('}', state);
-//  }
-//
-//  utils.move(node.body.range[0] + '{'.length, state);
-//  traverse(node.body, path, state);
-//  utils.catchupWhiteSpace(node.range[1], state);
-}
-
 
 visitReactJSXModuleDeclaration.test = function(node, path, state) {
   return node.type === Syntax.JSXModuleDeclaration;
